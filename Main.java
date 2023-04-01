51d8fead2d0fe6040c1188288b1a8cc9b0eb2d2c
Robert <az.roberto1998@gmail.com>
2023-04-01 20:32:12 +0400

    Stage 3/4: Score!

--- Main.java
+++ Main.java
@@ -1,19 +1,45 @@
 package readability;
 
-import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        int countOfSentences;
-        int countOfWords = 0;
 
-        String[] strArr = str.split("[.!?]");
-        countOfSentences = strArr.length;
-        for (String s : strArr) {
-            countOfWords += s.split(" ").length;
+        // Ввод имени файла для индексации через args.
+        StringBuilder strB = new StringBuilder();
+
+        if (args.length != 0) {
+            File file = new File(args[0]);
+            try (Scanner scanner = new Scanner(file)) {
+                while (scanner.hasNext()) {
+                    strB.append(scanner.nextLine());
+                }
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            String str = strB.toString();
+            int charsCount = new String(str).replace(" ", "").length();
+            int wordsCount = str.split(" ").length;
+            int sentencesCount = str.split("[.!?]").length;
+            double score = 4.71 * charsCount / wordsCount + 0.5 * wordsCount / sentencesCount - 21.43;
+            int age = (int)(Math.ceil(score) + 4);
+
+            System.out.printf("""
+                    The text is:
+                    %s
+                    
+                    Words: %d
+                    Sentences: %d
+                    Characters: %d
+                    The score is: %3.2f
+                    This text should be understood by %d-%d years-old.
+                    """,
+                    str, wordsCount, sentencesCount, charsCount, score, age, age + 1
+                    );
         }
-        System.out.println(countOfWords / countOfSentences > 10 ? "HARD" : "EASY");
     }
 }
